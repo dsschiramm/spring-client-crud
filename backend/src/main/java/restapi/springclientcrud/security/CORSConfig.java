deec5508612241101e0fc78decba8bc35b450f1d
Daniel Schiramm <dss.schiramm@gmail.com>
2020-10-14 00:52:05 -0300

    resolvendo concorrencia container mysql e spring

--- backend/src/main/java/restapi/springclientcrud/security/CORSConfig.java
+++ backend/src/main/java/restapi/springclientcrud/security/CORSConfig.java
@@ -1,25 +1,20 @@
 package restapi.springclientcrud.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CORSConfig {
+@EnableWebMvc
+public class CORSConfig implements WebMvcConfigurer  {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry
-                        .addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("*")
-                        .allowedHeaders("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedOrigins("*")
+            .allowedHeaders("*");
     }
 }
